1de65022f658d5e0e6b7434a6e1195215509d2ab
nebojsadj <djordjevicn87@gmail.com>
2020-10-08 18:04:42 +0200

    destructuring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Home({ state }) {
+  const { firstName, lastName } = state;
   return (
     <div>
-      <h1 className="text-center display-4">{`Welcome ${state.firstName} ${state.lastName} !`}</h1>
+      <h1 className="text-center display-4">{`Welcome ${firstName} ${lastName} !`}</h1>
     </div>
   );
 }
