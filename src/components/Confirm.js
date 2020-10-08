2a97d02ba9114990c53438bd55d98468e224c3d0
nebojsadj <djordjevicn87@gmail.com>
2020-10-08 17:02:24 +0200

    removed console.log

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Confirm({ state }) {
-  console.log(state);
   return (
     <div className="container">
       <div className="row">
