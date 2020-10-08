26942314db67f97a1086644932e03872806fe4eb
nebojsadj <djordjevicn87@gmail.com>
2020-10-08 17:59:12 +0200

    added h3 to list

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -11,13 +11,23 @@ function Confirm({ state }) {
           <h3 className="text-center">Step 3</h3>
           <ul className="list-group text-center">
             <li className="list-group-item mt-4">
-              {`FirstName: ${firstName}`}
+              <h3>{`FirstName: ${firstName}`}</h3>
             </li>
-            <li className="list-group-item">{`LastName: ${lastName}`}</li>
-            <li className="list-group-item">{`Email: ${email}`}</li>
-            <li className="list-group-item">{`Profession: ${profession}`}</li>
-            <li className="list-group-item">{`WorkStatus: ${workStatus}`}</li>
-            <li className="list-group-item">{`Phone: ${phone}`}</li>
+            <li className="list-group-item">
+              <h3>{`LastName: ${lastName}`}</h3>
+            </li>
+            <li className="list-group-item">
+              <h3>{`Email: ${email}`}</h3>
+            </li>
+            <li className="list-group-item">
+              <h3>{`Profession: ${profession}`}</h3>
+            </li>
+            <li className="list-group-item">
+              <h3>{`WorkStatus: ${workStatus}`}</h3>
+            </li>
+            <li className="list-group-item">
+              <h3>{`Phone: ${phone}`}</h3>
+            </li>
           </ul>
           <Link to="/details" className="btn btn-secondary form-control mt-3">
             Back
